59562cb713d85537eaa8719f2aca579c5f8b1959
trionoaja <trionoaja@gmail.com>
2026-03-31 13:20:41 +0700

    Update Kategori.php

--- application/controllers/Kategori.php
+++ application/controllers/Kategori.php
@@ -12,8 +12,8 @@ class Kategori extends CI_Controller {
     {
         $data['kategori'] = $this->Kategori_model->get_all();
 		$this->load->view('templates/header');
-		$this->load->view('templates/topbar');
 		$this->load->view('templates/sidebar');
+		$this->load->view('templates/topbar');
         $this->load->view('kategori/index', $data);
 		$this->load->view('templates/footer');
 
@@ -21,8 +21,8 @@ class Kategori extends CI_Controller {
     public function tambah()
     {
         $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
 		$this->load->view('templates/topbar');
-		$this->load->view('templates/sidebar');
         $this->load->view('kategori/tambah');
         $this->load->view('templates/footer');
     }
